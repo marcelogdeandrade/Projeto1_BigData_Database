098aef88873c05ce3f71d4aecde3f257b5b876c6
marcelogdeandrade@gmail.com <marcelogdeandrade@gmail.com>
2017-09-06 11:11:39 -0300

    does sprint1 and sprint2

--- sprint1.sql
+++ sprint1.sql
@@ -4,8 +4,8 @@ USE `OngAnimais`;
 
 DROP TABLE IF EXISTS `Species`;
 CREATE TABLE `Species` (
-	`idSpecie` bigint NOT NULL AUTO_INCREMENT,
+	`idSpecies` bigint NOT NULL AUTO_INCREMENT,
 	`name` varchar(20) NOT NULL,
 	`status` bool NOT NULL,
-	PRIMARY KEY (`idSpecie`)
+	PRIMARY KEY (`idSpecies`)
 );
--- sprint2.sql
+++ sprint2.sql
@@ -5,8 +5,42 @@ CREATE TABLE `Pets` (
 	`idPet` bigint NOT NULL AUTO_INCREMENT,
 	`name` varchar(20) NOT NULL,
 	`birthDate` DATE NOT NULL,
-	`idSpecie` bigint NOT NULL,
+	`idSpecies` bigint NOT NULL,
 	`idClient` bigint,
 	`status` bool NOT NULL,
 	PRIMARY KEY (`idPet`)
 );
+
+DROP TABLE IF EXISTS `Medicines`;
+CREATE TABLE `Medicines` (
+	`idMedicine` bigint NOT NULL AUTO_INCREMENT,
+	`name` varchar(20) NOT NULL,
+	`status` bool NOT NULL,
+	PRIMARY KEY (`idMedicine`)
+);
+
+DROP TABLE IF EXISTS `RelMedicinesPets`;
+CREATE TABLE `RelMedicinesPets` (
+	`idMedicine` bigint NOT NULL,
+	`idPet` bigint NOT NULL,
+	`quantity` int NOT NULL,
+	`nextDose` DATE NOT NULL,
+	`status` bool NOT NULL,
+	PRIMARY KEY (`idMedicine`,`idPet`)
+);
+
+DROP TABLE IF EXISTS `Clients`;
+CREATE TABLE `Clients` (
+	`idClient` bigint NOT NULL AUTO_INCREMENT,
+	`name` varchar(20) NOT NULL,
+	`birthDate` DATE NOT NULL,
+	`status` bool NOT NULL,
+	PRIMARY KEY (`idClient`)
+);
+
+ALTER TABLE `Pets` ADD CONSTRAINT `Pets_fk0` FOREIGN KEY (`idSpecies`) REFERENCES `Species`(`idSpecies`);
+ALTER TABLE `Pets` ADD CONSTRAINT `Pets_fk1` FOREIGN KEY (`idClient`) REFERENCES `Clients`(`idClient`);
+ALTER TABLE `RelMedicinesPets` ADD CONSTRAINT `RelMedicinesPets_fk0` FOREIGN KEY (`idMedicine`) REFERENCES `Medicines`(`idMedicine`);
+ALTER TABLE `RelMedicinesPets` ADD CONSTRAINT `RelMedicinesPets_fk1` FOREIGN KEY (`idPet`) REFERENCES `Pets`(`idPet`);
+
+
